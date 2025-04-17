52a1a0870a0d02c5fb3b64d26be1252b8344f9c0
angeloHbras <angelohenrique2004@gmail.com>
2025-04-17 15:58:24 +0100

    feat : UserBuilder implemented

---
+++ GetWatch/Services/UserBuilder.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GetWatch.Interfaces;
+using GetWatch.Services.Db;
+
+namespace GetWatch.Services
+{
+    public class UserBuilder : IUserBuilder
+    {
+       
+        
+    private DbUser _user = new DbUser();
+
+    public IUserBuilder SetUserName(string name)
+    {
+        _user.Name = name;
+        return this;
+    }
+
+    public IUserBuilder SetUserEmail(string email)
+    {
+        _user.Email = email;
+        return this;
+    }
+
+    public IUserBuilder SetUserPassword(string password)
+    {
+        _user.Password = password;
+        return this;
+    }
+
+    public DbUser BuildUser()
+    {
+        return _user;
+    }
+        
+
+
+    }
+}
