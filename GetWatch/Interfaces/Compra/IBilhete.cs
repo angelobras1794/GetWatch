aee7f55f38cee51f0ce516c93ad53a6e12749148
cality2004 <marcosafonsodasilvafreitas@gmail.com>
2025-05-09 14:58:29 +0100

    feat: implementation the Mapper in Bilhetes

--- GetWatch/Interfaces/Compra/IBilhete.cs
+++ GetWatch/Interfaces/Compra/IBilhete.cs
@@ -4,6 +4,9 @@ namespace GetWatch.Interfaces.Compra;
 
 public interface IBilhete
 {
+    public int PersonAmount { get; set; } 
+        public string []? Seats { get; set; }
+         public Guid Id { get; set; }
 
      decimal CalcularPreco();
     string Descrever();
